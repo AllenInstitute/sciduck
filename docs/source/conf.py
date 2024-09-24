04e80f3a17f9ef5763ccaf07c09526d7792a496a
UCDNJJ <njjohansen@ucdavis.edu>
2024-09-23 21:32:50 -0700

    rtd_docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# pip install sphinx-rtd-theme; https://sphinx-themes.org/sample-sites/sphinx-rtd-theme/
 
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
