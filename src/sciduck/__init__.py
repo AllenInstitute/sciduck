1f1c315f68ce1dcd729ff7e18fa13d1e92732722
UCDNJJ <njjohansen@ucdavis.edu>
2024-11-26 12:06:50 -0800

    updates

--- src/sciduck/__init__.py
+++ src/sciduck/__init__.py
@@ -4,7 +4,7 @@ sciduck
 single cell data analyis unification and communication toolkit
 """
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __authors__ = ['Nelson Johansen', 'Matthew Schmitz', 'Dan Yuan']
 __credits__ = 'Allen Institute for Brain Science'
 
