d47c3bb958a4f531d715990a8141583d23ff563e
UCDNJJ <njjohansen@ucdavis.edu>
2025-02-13 10:52:33 -0800

    anno_table

--- src/sciduck/__init__.py
+++ src/sciduck/__init__.py
@@ -10,5 +10,6 @@ __credits__ = 'Allen Institute for Brain Science'
 
 from . import basic_qc as qc
 from . import plotting as pl
-from . import annotation_table
-from . import entropy_qc
+from . import annotation_table as at
+from . import entropy_qc as eq
+from . import templates as template
